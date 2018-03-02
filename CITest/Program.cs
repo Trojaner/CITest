277aca97db0684644e1cdfc384ae82e998b2ca08
Trojaner_ <trojaner25@gmail.com>
2018-03-02 11:32:54 +0300

    Initialize commit

---
+++ CITest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CITest/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace CITest
+{
+    public class UnitTests
+    {
+        [Fact(DisplayName = "Fail test")]
+        public void Fail_Test()
+        {
+            Assert.True(false, "This test should fail");
+        }
+
+        [Fact(DisplayName = "Pass test")]
+        public void Pass_Test()
+        {
+            Assert.True(true, "This test should not happen");
+        }
+    }
+}
